57b5efd349f566995a41cc6976967ba1bdd2ce32
elizabethbright721 <elizabethbright721@gmail.com>
2023-05-10 11:18:57 -0400

    Pages layout ppartially complete awaiting Components

---
+++ src/Pages/Edit.js
@@ -0,0 +1,11 @@
+
+function Edit() {
+    return (
+      <div className="New Edit">
+        <h2>Edit</h2>
+        
+      </div>
+    );
+  }
+  
+  export default Edit;
---
+++ src/Pages/New.js
@@ -0,0 +1,10 @@
+function New() {
+    return (
+      <div className="New">
+        <h2>New</h2>
+        
+      </div>
+    );
+  }
+  
+  export default New;
