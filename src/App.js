40e473a497bfdda57e8000c6f23a2b677b4c43fd
Wilton <wiltoncamilol@gmail.com>
2023-05-11 10:27:22 -0400

    NavBar and App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './Components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './Components/Navbar';
 // import Home from './Home';
 // import About from './About';
-// import Contact from './Contact';
 
-function App() {
+
+const App = () => {
   return (
     <Router>
-      <div>
-        <Navbar />
-        {/* <Routes> */}
-          {/* <Route path="/" exact component={Home} />
+      <NavBar />
+      <div className="container">
+        {/* <Routes>
+          <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} /> */}
-        {/* </Routes> */}
+          <Route path="/latest-trend" component={LatestTrend} />
+        </Routes> */}
       </div>
     </Router>
   );
